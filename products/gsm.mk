b7360d297b4bdfaf979bbf0dd1f80e97f18e1310
Yang Hong <yangh.cn@gmail.com>
2010-10-31 00:47:38 +0800

    Add operators.xml file for customize operator name
    Change-Id: I95e8c3863238696f51828d85d071025f1281933e

--- products/gsm.mk
+++ products/gsm.mk
@@ -2,6 +2,10 @@
 PRODUCT_COPY_FILES += \
     vendor/cyanogen/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
+# Operator info list
+PRODUCT_COPY_FILES += \
+    vendor/cyanogen/prebuilt/common/etc/operators.xml:system/etc/operators.xml
+
 # SIM Toolkit
 PRODUCT_PACKAGES += \
     Stk
