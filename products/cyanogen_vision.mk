cfc9fab338205ce3c55d476a486afa1826e2bf96
Steve Kondik <shade@chemlab.org>
2010-12-08 02:15:22 -0500

    vision: dotone
    Change-Id: Ia8fcbe37b6b8b0fa5522579482858f3ae05d9895

--- products/cyanogen_vision.mk
+++ products/cyanogen_vision.mk
@@ -38,7 +38,7 @@ ifdef CYANOGEN_NIGHTLY
         ro.modversion=CyanogenMod-6-$(shell date +%m%d%Y)-NIGHTLY-vision
 else
     PRODUCT_PROPERTY_OVERRIDES += \
-        ro.modversion=CyanogenMod-6.1.0-vision
+        ro.modversion=CyanogenMod-6.1.1-vision
 endif
 
 #
