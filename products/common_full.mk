471b18163863ef637bbe4f245aa3fd2b0bf2eb22
Abaakouk Mehdi <theli48@gmail.com>
2011-01-22 16:26:18 +0100

    Add TTS pico languages only for full product
    Change-Id: Iff5a467eeed19e0a56cc31054ba83a9aa4e1056e

--- products/common_full.mk
+++ products/common_full.mk
@@ -7,6 +7,9 @@ include frameworks/base/data/sounds/AllAudio.mk
 # Theme packages
 include vendor/cyanogen/products/themes.mk
 
+# Get the TTS language packs
+$(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
+
 # Default ringtone
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=DonMessWivIt.ogg
