044be02d3de2944bedc3dfadcf49134f8bdeb1d8
Jacob Crawley <jacobcrawley@gmail.com>
2011-02-09 14:28:43 -0500

    DO NOT MERGE. Add ZTE Blade, probably needs some tweaking.
    Change-Id: Ibd57a6ad4cf873852c81c92c0e9eba4a57e28f46

---
+++ products/cyanogen_blade.mk
@@ -0,0 +1,46 @@
+# Inherit AOSP device configuration for Blade.
+$(call inherit-product, device/zte/blade/device_blade.mk)
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/cyanogen/products/common_full.mk)
+
+# Include GSM stuff
+$(call inherit-product, vendor/cyanogen/products/gsm.mk)
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := cyanogen_blade
+PRODUCT_BRAND := ZTE
+PRODUCT_DEVICE := blade
+PRODUCT_MODEL := ZTE Blade
+PRODUCT_MANUFACTURER := ZTE
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=zte_Blade BUILD_ID=FRG83 BUILD_DISPLAY_ID=GRH78C BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2.1/FRG83/60505:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.2.1 FRG83 60505 release-keys"
+
+#
+# Inherit 2G VMSPLIT prelink map. I plan to phase this out with a kernel
+# update eventually 
+#
+PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/vendor/cyanogen/prelink-linux-arm-2G-VMSPLIT.map
+
+#
+# Set ro.modversion
+#
+ifdef CYANOGEN_NIGHTLY
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.modversion=CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-Blade
+else
+    ifdef CYANOGEN_RELEASE
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.0.0-RC0-Blade
+    else
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.0.0-RC0-Blade-KANG
+    endif
+endif
+
+#
+# Copy legend specific prebuilt files
+#
+PRODUCT_COPY_FILES +=  \
+    vendor/cyanogen/prebuilt/hdpi/media/bootanimation.zip:system/media/bootanimation.zip
