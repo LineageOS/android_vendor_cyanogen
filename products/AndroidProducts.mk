f96e0075477a5cf499b08868f794bff874cb8fff
Tom Giordano <tomgiordano83@gmail.com>
2011-03-08 00:22:17 +1100

    Add Huawei U8220/U8230
    The U8220 and U8230 are the same device. This should be compatible with both, but so far has only been tested on U8220 (aka T-Mobile Pulse).
    
    device config should be forked from https://github.com/TomGiordano/android_device_huawei_u8220
    
    Change-Id: If0141d63d8d41360a9cfa69283e5bdbda17c1b8f

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -23,6 +23,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_sholes.mk \
     $(LOCAL_DIR)/cyanogen_speedy.mk \
     $(LOCAL_DIR)/cyanogen_supersonic.mk \
+    $(LOCAL_DIR)/cyanogen_u8220.mk \
     $(LOCAL_DIR)/cyanogen_vega.mk \
     $(LOCAL_DIR)/cyanogen_vision.mk \
     $(LOCAL_DIR)/cyanogen_z71.mk \
