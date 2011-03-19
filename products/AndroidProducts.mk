namdeptrai2003/android_vendor_dnd

Add ZERO product
Change-Id: I208af430c4a5f17c45b6276e166a25a349c19a7f

products/AndroidProducts.mk
@@ -21,9 +21,9 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_one.mk \
     $(LOCAL_DIR)/cyanogen_passion.mk \
     $(LOCAL_DIR)/cyanogen_sholes.mk \
+    $(LOCAL_DIR)/cyanogen_speedy.mk \
     $(LOCAL_DIR)/cyanogen_supersonic.mk \
     $(LOCAL_DIR)/cyanogen_vega.mk \
     $(LOCAL_DIR)/cyanogen_vision.mk \
     $(LOCAL_DIR)/cyanogen_z71.mk \
-    $(LOCAL_DIR)/cyanogen_speedy.mk
-
+    $(LOCAL_DIR)/cyanogen_zero.mk

LineageOS/android_vendor_cyanogen

products/AndroidProducts.mk
@@ -24,4 +24,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_supersonic.mk \
     $(LOCAL_DIR)/cyanogen_vega.mk \
     $(LOCAL_DIR)/cyanogen_vision.mk \
-    $(LOCAL_DIR)/cyanogen_z71.mk
+    $(LOCAL_DIR)/cyanogen_z71.mk \
+    $(LOCAL_DIR)/cyanogen_zero.mk