14e70bf34981d3afe17fb267cb7138bd4b12c396
Emilio López <turl@tuxfamily.org>
2011-04-04 20:04:14 +0000

    Add zeppelin files
    Change-Id: Id59c0a7a6d0784e6caf0b81fff160fc7f9eec6e2

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -27,5 +27,6 @@ add_lunch_combo cyanogen_vega-eng
 add_lunch_combo cyanogen_vision-eng
 add_lunch_combo cyanogen_z71-eng
 add_lunch_combo cyanogen_zero-eng
+add_lunch_combo cyanogen_zeppelin-eng
 
 PATH=$PATH:$PWD/vendor/cyanogen/tools ; export PATH
