6c344691c6bc8d9ff38b66652ec451101f85029a
Brint E. Kriebel <github@bekit.net>
2011-04-11 15:52:09 -0700

    harmony: branched harmony to malata harmony_gtablet and harmony_t8
    Change-Id: I2adea07f09ac8b5fd516ec0b3b462ce6ddbbe950

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -15,7 +15,8 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_inc.mk \
     $(LOCAL_DIR)/cyanogen_generic.mk \
     $(LOCAL_DIR)/cyanogen_glacier.mk \
-    $(LOCAL_DIR)/cyanogen_harmony.mk \
+    $(LOCAL_DIR)/cyanogen_harmony_t8.mk \
+    $(LOCAL_DIR)/cyanogen_harmony_gtablet.mk \
     $(LOCAL_DIR)/cyanogen_legend.mk \
     $(LOCAL_DIR)/cyanogen_leo.mk \
     $(LOCAL_DIR)/cyanogen_liberty.mk \
--- products/cyanogen_harmony_gtablet.mk
+++ products/cyanogen_harmony_gtablet.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/nvidia/harmony/device_harmony.mk)
+$(call inherit-product, device/malata/harmony_gtablet/harmony_gtablet.mk)
 
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cyanogen/products/common_full.mk)
@@ -6,9 +6,9 @@ $(call inherit-product, vendor/cyanogen/products/common_full.mk)
 #
 # Setup device specific product configuration.
 #
-PRODUCT_NAME := cyanogen_harmony
+PRODUCT_NAME := cyanogen_harmony_gtablet
 PRODUCT_BRAND := nvidia
-PRODUCT_DEVICE := harmony
+PRODUCT_DEVICE := harmony_gtablet
 PRODUCT_MODEL := GTablet
 PRODUCT_MANUFACTURER := malata
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=harmony BUILD_ID=GRI40 BUILD_DISPLAY_ID=GRI40 BUILD_FINGERPRINT=google/passion/passion:2.3.3/GRI40/102588:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.3 GRI40 102588 release-keys"
@@ -16,20 +16,21 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=harmony BUILD_ID=GRI40 BUILD_DISPLA
 # Extra overlay
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/cyanogen/overlay/tablet \
-    vendor/cyanogen/overlay/harmony
+    vendor/cyanogen/overlay/harmony_gtablet
+
 #
 # Set ro.modversion
 #
 ifdef CYANOGEN_NIGHTLY
     PRODUCT_PROPERTY_OVERRIDES += \
-        ro.modversion=CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-Harmony
+        ro.modversion=CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-Harmony-GTablet
 else
     ifdef CYANOGEN_RELEASE
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.0.0-Harmony
+            ro.modversion=CyanogenMod-7.0.0-Harmony-GTablet
     else
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.0.0-Harmony-KANG
+            ro.modversion=CyanogenMod-7.0.0-Harmony-GTablet-KANG
     endif
 endif
 
