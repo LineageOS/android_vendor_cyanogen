7d8dc59d1d0c67ffcfd9aed9768e610fc065ca2c
Brint E. Kriebel <github@bekit.net>
2011-04-29 16:19:28 -0700

    harmony: renamed harmony to malata smb_a1002 (GTablet)
    Change-Id: I2adea07f09ac8b5fd516ec0b3b462ce6ddbbe950

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,7 +11,6 @@ add_lunch_combo cyanogen_encore-eng
 add_lunch_combo cyanogen_espresso-eng
 add_lunch_combo cyanogen_generic-eng
 add_lunch_combo cyanogen_glacier-eng
-add_lunch_combo cyanogen_harmony-eng
 add_lunch_combo cyanogen_hero-eng
 add_lunch_combo cyanogen_heroc-eng
 add_lunch_combo cyanogen_inc-eng
@@ -22,6 +21,7 @@ add_lunch_combo cyanogen_one-eng
 add_lunch_combo cyanogen_passion-eng
 add_lunch_combo cyanogen_p999-eng
 add_lunch_combo cyanogen_sholes-eng
+add_lunch_combo cyanogen_smb_a1002-eng
 add_lunch_combo cyanogen_speedy-eng
 add_lunch_combo cyanogen_supersonic-eng
 add_lunch_combo cyanogen_vega-eng
