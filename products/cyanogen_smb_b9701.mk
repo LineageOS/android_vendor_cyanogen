f5e1e35595c9334ae231cf9ea54dc24927cb58fa
Brint E. Kriebel <github@bekit.net>
2011-04-29 16:23:24 -0700

    harmony: forked original harmony (smb_a1002) to new malata smb9701 (Zpad T8) device
    Change-Id: I7855585cde3375e30ceb11c1030e2e283126f64e

---
+++ products/cyanogen_smb_b9701.mk
@@ -0,0 +1,41 @@
+$(call inherit-product, device/malata/smb_b9701/smb_b9701.mk)
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/cyanogen/products/common_full.mk)
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := cyanogen_smb_b9701
+PRODUCT_BRAND := nvidia
+PRODUCT_DEVICE := smb_b9701
+PRODUCT_MODEL := Zpad-T8
+PRODUCT_MANUFACTURER := malata
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=harmony BUILD_ID=GRI40 BUILD_DISPLAY_ID=GRI40 BUILD_FINGERPRINT=google/passion/passion:2.3.3/GRI40/102588:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.3 GRI40 102588 release-keys"
+
+# Extra overlay
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/cyanogen/overlay/tablet \
+    vendor/cyanogen/overlay/smb_b9701
+
+#
+# Set ro.modversion
+#
+ifdef CYANOGEN_NIGHTLY
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.modversion=CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-smb_b9701
+else
+    ifdef CYANOGEN_RELEASE
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.1.0-RC0-smb_b9701
+    else
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.1.0-RC0-smb_b9701-KANG
+    endif
+endif
+
+#
+# Copy passion specific prebuilt files
+#
+PRODUCT_COPY_FILES +=  \
+    vendor/cyanogen/prebuilt/mdpi/media/bootanimation.zip:system/media/bootanimation.zip
