b30a3b748e00116b93c471f6451c30b7e220bc53
tilal6991 <tilal6991@gmail.com>
2011-07-04 13:39:26 +0100

    This commit adds the device manifest for the Ideos.
    I request that the project for the Ideos be added to Gerrit or atleast accept my pull request for the device config on Github. Thanks!
    
    This commit fixes the things as asked by Danny.
    
    Change-Id: I5e6d10b5113b52e519b4920d1f58a80b2526b222

--- products/cyanogen_ideos.mk
+++ products/cyanogen_ideos.mk
@@ -26,10 +26,10 @@ ifdef CYANOGEN_NIGHTLY
 else
     ifdef CYANOGEN_RELEASE
         PRODUCT_PROPERTY_OVERRIDES += \
-           ro.modversion=CyanogenMod-7.1.0-RC1-IDEOS
+          ro.modversion=CyanogenMod-7.1.0-RC1-IDEOS
     else
         PRODUCT_PROPERTY_OVERRIDES += \
-           ro.modversion=CyanogenMod-7.1.0-RC1-IDEOS-KANG
+          ro.modversion=CyanogenMod-7.1.0-RC1-IDEOS-KANG
     endif
 endif
 
