7560e1377825f5768961ad18dac4dbeadddf0373
Alex Zepeda <alex@inferiorhumanorgans.com>
2011-07-16 12:33:02 -0700

    Add the Cricket stubs. Still waiting on a stock Cricket ROM and working kernel.
    Change-Id: I87f38c9eeff908526bbda20133ec04de4f206b9b

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,7 +1,8 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_thunderc.mk \
-    $(LOCAL_DIR)/cyanogen_thunderc_virgin.mk \
-    $(LOCAL_DIR)/cyanogen_thunderc_sprint.mk \
+    $(LOCAL_DIR)/cyanogen_thunderc_cricket.mk \
     $(LOCAL_DIR)/cyanogen_thunderc_metropcs.mk \
+    $(LOCAL_DIR)/cyanogen_thunderc_sprint.mk \
     $(LOCAL_DIR)/cyanogen_thunderc_usc.mk \
-    $(LOCAL_DIR)/cyanogen_thunderc_verizon.mk
+    $(LOCAL_DIR)/cyanogen_thunderc_verizon.mk \
+    $(LOCAL_DIR)/cyanogen_thunderc_virgin.mk
