ac7a697b1db461c0ef9dee3eaef4c4e11a5052e0
Charan Singh <charansingh.online@gmail.com>
2011-07-31 05:01:30 +0530

    Add Evo 3D (Shooter)
    Change-Id: I22717eb347a7438f08302e65076113b336a3ed4d

---
+++ products/cyanogen_shooter.mk
@@ -0,0 +1,54 @@
+# Inherit AOSP device configuration for shooter.
+$(call inherit-product, device/htc/shooter/shooter.mk)
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/cyanogen/products/common_full.mk)
+
+# Inherit WiMAX stuff
+$(call inherit-product, vendor/cyanogen/products/wimax.mk)
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := cyanogen_shooter
+PRODUCT_BRAND := sprint
+PRODUCT_DEVICE := shooter
+PRODUCT_MODEL := PG8610000
+PRODUCT_MANUFACTURER := HTC
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=GRI40 BUILD_DISPLAY_ID=GRJ90 PRODUCT_NAME=htc_shooter BUILD_FINGERPRINT=sprint/htc_shooter/shooter:2.3.3/GRI40/83920:user/release-keys
+
+# Build kernel
+#PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL=
+#PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_DIR=kernel-msm
+#PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_CONFIG=cyanogen_shooter_defconfig
+
+# Extra shooter overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/shooter
+
+# Add the Torch app
+PRODUCT_PACKAGES += Torch
+
+# Broadcom FM radio
+$(call inherit-product, vendor/cyanogen/products/bcm_fm_radio.mk)
+
+#
+# Set ro.modversion
+#
+ifdef CYANOGEN_NIGHTLY
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.modversion=CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-shooter
+else
+    ifdef CYANOGEN_RELEASE
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.1.0-RC1-shooter
+    else
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.1.0-RC1-shooter-KANG
+    endif
+endif
+
+#
+# Copy passion specific prebuilt files
+#
+PRODUCT_COPY_FILES +=  \
+    vendor/cyanogen/prebuilt/hdpi/media/bootanimation.zip:system/media/bootanimation.zip
