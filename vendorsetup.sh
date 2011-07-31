216b9caf2939e647c77e6afb10ce7023fe87db3c
Lalit Maganti <tilal6991@gmail.com>
2011-07-31 19:02:07 +0100

    Adds U8150 to build tree.
    Change-Id: Ibc586ed334dd0988f4d25db06a9447c8abfd3333

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -38,6 +38,7 @@ add_lunch_combo cyanogen_smb_a1011-eng
 add_lunch_combo cyanogen_smb_b9701-eng
 add_lunch_combo cyanogen_speedy-eng
 add_lunch_combo cyanogen_supersonic-eng
+add_lunch_combo cyanogen_u8150-eng
 add_lunch_combo cyanogen_u8220-eng
 add_lunch_combo cyanogen_vega-eng
 add_lunch_combo cyanogen_vibrantmtd-eng
