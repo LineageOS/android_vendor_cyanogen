22fd10969a00042187eb5c21c2ae4c3aa91ae3f5
jerpelea <jerpelea@gmail.com>
2011-08-06 18:34:58 +0300

    cyanogen: Add SEMC X10i (Xperia X10)
    Change-Id: I2b47276f5451387653caef77e80742424062f716

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,6 +12,7 @@ add_lunch_combo cyanogen_desirec-eng
 add_lunch_combo cyanogen_dream_sapphire-eng
 add_lunch_combo cyanogen_droid2-eng
 add_lunch_combo cyanogen_encore-eng
+add_lunch_combo cyanogen_es209ra-eng
 add_lunch_combo cyanogen_espresso-eng
 add_lunch_combo cyanogen_fascinatemtd-eng
 add_lunch_combo cyanogen_galaxys2-eng
