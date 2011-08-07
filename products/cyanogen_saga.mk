22f91aa8a753d17bbe628ccceb8a3423700173ab
Alexander Zinn <zinnalex@gmail.com>
2011-08-07 18:04:18 +0000

    Add support to Desire S (saga) device
    My device url is : https://github.com/SuperKid
    
    Change-Id: If390543d8fc5e35a5f8ad84e547aee0c685e7cff

---
+++ products/cyanogen_saga.mk
@@ -0,0 +1,58 @@
+# Inherit device configuration for saga.
+$(call inherit-product, device/htc/saga/saga.mk)
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/cyanogen/products/common_full.mk)
+
+# Include GSM stuff
+$(call inherit-product, vendor/cyanogen/products/gsm.mk)
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := cyanogen_saga
+PRODUCT_BRAND := htc_wwe
+PRODUCT_DEVICE := saga
+PRODUCT_MODEL := Desire S
+PRODUCT_MANUFACTURER := HTC
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_saga BUILD_ID=GRI40 BUILD_DISPLAY_ID=GRJ90 BUILD_FINGERPRINT=htc_wwe/htc_saga/saga:2.3.3/GRI40/88716:user/release-keys PRIVATE_BUILD_DESC="2.30.405.1 CL88716 release-keys"
+
+# Build kernel
+#PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL=
+#PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_DIR=kernel-msm
+#PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_CONFIG=cyanogen_saga_defconfig
+
+# Extra saga overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/saga
+
+# Add the Torch app
+PRODUCT_PACKAGES += Torch
+
+# Add AicBootFix for saga battery fix
+PRODUCT_PACKAGES += AicBootFix
+
+# Broadcom FM radio
+$(call inherit-product, vendor/cyanogen/products/bcm_fm_radio.mk)
+
+#
+# Set ro.modversion
+#
+ifdef CYANOGEN_NIGHTLY
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.modversion=CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-IncS
+else
+    ifdef CYANOGEN_RELEASE
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.1.0-RC1-DS
+    else
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.1.0-RC1-DS-KANG
+
+    endif
+endif
+
+#
+# Copy passion specific prebuilt files
+#
+PRODUCT_COPY_FILES +=  \
+    vendor/cyanogen/prebuilt/hdpi/media/bootanimation.zip:system/media/bootanimation.zip
