363ffe7006243b624bcfc3053175171c9b8cc591
jerpelea <jerpelea@gmail.com>
2011-08-11 21:01:53 +0300

    cyanogen: Add SEMC E10i (Xperia X10Mini)
    Change-Id: Id5d8e8eb091f43746ad43360230cb629739a8064

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -36,8 +36,10 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_p990.mk \
     $(LOCAL_DIR)/cyanogen_p999.mk \
     $(LOCAL_DIR)/cyanogen_passion.mk \
+    $(LOCAL_DIR)/cyanogen_robyn.mk \
     $(LOCAL_DIR)/cyanogen_saga.mk \
     $(LOCAL_DIR)/cyanogen_shadow.mk \
+    $(LOCAL_DIR)/cyanogen_shakira.mk \
     $(LOCAL_DIR)/cyanogen_sholes.mk \
     $(LOCAL_DIR)/cyanogen_showcasemtd.mk \
     $(LOCAL_DIR)/cyanogen_smb_a1002.mk \
