2928f02c0ccc229824b4d0f16a924a798ab7f407
Diogo Ferreira <defer@cyanogenmod.com>
2011-10-10 22:29:15 +0100

    vendor: Bump vibrantmtd to 7.1.0.1
    This bump will fix some missing proprietary files which caused
    GPS to fail.
    
    Change-Id: Id5b5770a9ffa237bc51689331b01f3fee82f486c

--- products/cyanogen_vibrantmtd.mk
+++ products/cyanogen_vibrantmtd.mk
@@ -48,10 +48,10 @@ ifdef CYANOGEN_NIGHTLY
 else
     ifdef CYANOGEN_RELEASE
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0-Vibrant
+            ro.modversion=CyanogenMod-7.1.0.1-Vibrant
     else
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0-Vibrant-KANG
+            ro.modversion=CyanogenMod-7.1.0.1-Vibrant-KANG
     endif
 endif
 
