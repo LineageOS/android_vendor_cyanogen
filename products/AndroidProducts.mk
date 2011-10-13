21acd32e08d459fa4d17b6ec5bce407fa6fdebeb
Diogo Ferreira <defer@cyanogenmod.com>
2011-10-13 20:50:07 +0100

    vendor: Add iyokan (Sony Ericsson Xperia Pro MK16i / MK16a)
    Change-Id: I1c18c712337776221c83613c4b1bd28034dda975

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -66,6 +66,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_vision.mk \
     $(LOCAL_DIR)/cyanogen_vivo.mk \
     $(LOCAL_DIR)/cyanogen_vivow.mk \
+    $(LOCAL_DIR)/cyanogen_iyokan.mk \
     $(LOCAL_DIR)/cyanogen_z71.mk \
     $(LOCAL_DIR)/cyanogen_zeppelin.mk \
     $(LOCAL_DIR)/cyanogen_zero.mk \
