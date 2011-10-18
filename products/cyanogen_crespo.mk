9f2775a8ec10a60f824b52643b41322bbebb88b8
KalimochoAz <calimochoazucarado@gmail.com>
2011-10-18 21:11:06 +0200

    Change release version to 7.1.0.1 for bugfix
    Change-Id: I60e4cda17d7467e936dcc7b5a4d3317ff660edf6

--- products/cyanogen_crespo.mk
+++ products/cyanogen_crespo.mk
@@ -15,7 +15,7 @@ PRODUCT_BRAND := google
 PRODUCT_DEVICE := crespo
 PRODUCT_MODEL := Nexus S
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=soju BUILD_ID=GRJ22 BUILD_DISPLAY_ID=GWK74 BUILD_FINGERPRINT=google/soju/crespo:2.3.4/GRJ22/121341:user/release-keys PRIVATE_BUILD_DESC="soju-user 2.3.4 GRJ22 121341 release-keys" BUILD_NUMBER=121341
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=soju BUILD_ID=GWK74 BUILD_FINGERPRINT=google/soju/crespo:2.3.7/GWK74/185293:user/release-keys PRIVATE_BUILD_DESC="soju-user 2.3.7 GWK74 185293 release-keys" BUILD_NUMBER=185293
 
 # Extra Crespo overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/crespo
@@ -38,10 +38,10 @@ ifdef CYANOGEN_NIGHTLY
 else
     ifdef CYANOGEN_RELEASE
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0-NS
+            ro.modversion=CyanogenMod-7.1.0.1-NS
     else
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0-NS-KANG
+            ro.modversion=CyanogenMod-7.1.0.1-NS-KANG
     endif
 endif
 
