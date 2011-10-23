3ead35e1f0510cf3ec4f93ad91dbd072c02ac218
jerpelea <jerpelea@gmail.com>
2011-10-23 16:34:06 +0300

    semc: updated market keys and rom version
    Change-Id: Ibf4c09c9184c3ad22d31365106a864fedaf22ef1

--- products/cyanogen_iyokan.mk
+++ products/cyanogen_iyokan.mk
@@ -1,37 +1,39 @@
-# Inherit device configuration for zeusc.
-$(call inherit-product, device/semc/zeusc/device_zeusc.mk)
+# Inherit device configuration for iyokan.
+$(call inherit-product, device/semc/iyokan/device_iyokan.mk)
 
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cyanogen/products/common_full.mk)
 
+# Include GSM stuff
+$(call inherit-product, vendor/cyanogen/products/gsm.mk)
 
 #
 # Setup device specific product configuration.
 #
-PRODUCT_NAME := cyanogen_zeusc
+PRODUCT_NAME := cyanogen_iyokan
 PRODUCT_BRAND := SEMC
-PRODUCT_DEVICE := zeusc
-PRODUCT_MODEL := R800x
+PRODUCT_DEVICE := iyokan
+PRODUCT_MODEL := MK16i
 PRODUCT_MANUFACTURER := Sony Ericsson
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=Play BUILD_ID=3.0.1.A.0.145 BUILD_FINGERPRINT=SEMC/R800i_1247-6199/R800i:2.3.3/3.0.1.A.0.145/bn_p:user/release-keys PRIVATE_BUILD_DESC="LT15i-user 2.3.3 3.0.1.A.0.145 bn_P test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=MK16i BUILD_ID=4.0.1.A.0.283 BUILD_FINGERPRINT=SEMC/MK16i_1247-1061/MK16i:2.3.4/4.0.1.A.0.283/bn_P:user/release-keys PRIVATE_BUILD_DESC="MK16i-user 2.3.4 4.0.1.A.0.283 bn_P test-keys"
 
 # Build kernel
 #PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL=
 #PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_DIR=kernel-msm
-#PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_CONFIG=cyanogen_zeus_defconfig
+#PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_CONFIG=cyanogen_iyokan_defconfig
 
-# Extra zeus overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/zeusc
+# Extra iyokan overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/iyokan
 
 # Add the Torch app
-#PRODUCT_PACKAGES += Torch
+PRODUCT_PACKAGES += Torch
 
 
 # BCM FM radio
 #$(call inherit-product, vendor/cyanogen/products/bcm_fm_radio.mk)
 
 # Release name and versioning
-PRODUCT_RELEASE_NAME := XperiaPlayCDMA-R800x
+PRODUCT_RELEASE_NAME := XperiaPro-MK16i
 PRODUCT_VERSION_DEVICE_SPECIFIC :=
 -include vendor/cyanogen/products/common_versions.mk
 
