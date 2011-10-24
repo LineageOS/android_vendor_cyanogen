373d3d6ac596221c26b5e5b285fca369f1a82b09
Diogo Ferreira <defer@cyanogenmod.com>
2011-10-24 09:00:31 +0100

    vendor: Push semc 2011 products to 7.1.0.2
    These products shipped with a RIL that will select the wrong
    application id for a small subset of users. This release will
    address this issue.
    
    Change-Id: If274b656820996ec1aad703aec0982f7b735eb63

--- products/cyanogen_smultron.mk
+++ products/cyanogen_smultron.mk
@@ -41,10 +41,10 @@ ifdef CYANOGEN_NIGHTLY
 else
     ifdef CYANOGEN_RELEASE
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0.1-XperiaMini-ST15i
+            ro.modversion=CyanogenMod-7.1.0.2-XperiaMini-ST15i
     else
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0.1-XperiaMini-ST15i-KANG
+            ro.modversion=CyanogenMod-7.1.0.2-XperiaMini-ST15i-KANG
     endif
 endif
 
