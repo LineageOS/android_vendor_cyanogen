a42da130069b656c90942640ad9262217d7f04d8
Atin M <atinm.dev@gmail.com>
2011-12-12 06:50:03 -0500

    Removed Vibrant from list of CyanogenMod devices
    Change-Id: I85cf03c8824ea7a8f9f5b94e63fc63b4af847cd5

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -65,7 +65,6 @@ add_lunch_combo cyanogen_u8220-eng
 add_lunch_combo cyanogen_urushi-eng
 add_lunch_combo cyanogen_v9-eng
 add_lunch_combo cyanogen_vega-eng
-add_lunch_combo cyanogen_vibrantmtd-eng
 add_lunch_combo cyanogen_vision-eng
 add_lunch_combo cyanogen_vivo-eng
 add_lunch_combo cyanogen_vivow-eng
