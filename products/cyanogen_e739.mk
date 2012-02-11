41f7854a53bac19b251755067adba57ffa586106
Giulio Cervera <giulio.cervera@gmail.com>
2012-02-11 14:24:43 +0100

    cm: Add E739 (T-Mobile LG myTouch)
    Change-Id: Ib99cea0e53de04b296837b57fc7445901de20bd8

--- products/cyanogen_e739.mk
+++ products/cyanogen_e739.mk
@@ -1,8 +1,8 @@
 # Include GSM stuff
 $(call inherit-product, vendor/cyanogen/products/gsm.mk)
 
-# Inherit device configuration for lge e730.
-$(call inherit-product, device/lge/e730/e730.mk)
+# Inherit device configuration for lge e739.
+$(call inherit-product, device/lge/e739/e739.mk)
 
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cyanogen/products/common_full.mk)
@@ -10,10 +10,10 @@ $(call inherit-product, vendor/cyanogen/products/common_full.mk)
 #
 # Setup device specific product configuration.
 #
-PRODUCT_NAME := cyanogen_e730
+PRODUCT_NAME := cyanogen_e739
 PRODUCT_BRAND := lge
-PRODUCT_DEVICE := e730
-PRODUCT_MODEL := LG-E730
+PRODUCT_DEVICE := e739
+PRODUCT_MODEL := LG-E739
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lge_victor BUILD_ID=GRJ22 BUILD_FINGERPRINT=lge/lge_victor/victor:2.3.4/GRJ22/V10a-OCT-05-2011.2ED317DCA9:user/release-keys PRIVATE_BUILD_DESC="lge_victor-user 2.3.4 GRJ22 V10a-OCT-05-2011.2ED317DCA9 release-keys"
 
@@ -21,7 +21,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lge_victor BUILD_ID=GRJ22 BUILD_FIN
 PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/victor-common
 
 # Release name and versioning
-PRODUCT_RELEASE_NAME := OptimusSol
+PRODUCT_RELEASE_NAME := LGmyTouch
 PRODUCT_VERSION_DEVICE_SPECIFIC :=
 -include vendor/cyanogen/products/common_versions.mk
 
