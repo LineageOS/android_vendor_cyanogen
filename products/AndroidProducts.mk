e0580d61864bb5a25e89700650201cc66a16bc9d
Renaud Lepage <root@cybikbase.com>
2012-04-21 02:24:39 -0400

    Motorola Photon 4G device [Codename: Sunfire]
    Change-Id: I7821ac87790bd2e01093e0a8ef67f193ae200342

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -54,12 +54,14 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_shakira.mk \
     $(LOCAL_DIR)/cyanogen_sholes.mk \
     $(LOCAL_DIR)/cyanogen_showcasemtd.mk \
+    $(LOCAL_DIR)/cyanogen_sunfire.mk \
     $(LOCAL_DIR)/cyanogen_smb_a1002.mk \
     $(LOCAL_DIR)/cyanogen_smb_a1004.mk \
     $(LOCAL_DIR)/cyanogen_smb_a1011.mk \
     $(LOCAL_DIR)/cyanogen_smb_b9701.mk \
     $(LOCAL_DIR)/cyanogen_smultron.mk \
     $(LOCAL_DIR)/cyanogen_speedy.mk \
+    $(LOCAL_DIR)/cyanogen_sunfire.mk \
     $(LOCAL_DIR)/cyanogen_supersonic.mk \
     $(LOCAL_DIR)/cyanogen_tass.mk \
     $(LOCAL_DIR)/cyanogen_u8150.mk \
