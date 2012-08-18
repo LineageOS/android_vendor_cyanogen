9ef61acdc4820551ae2aa69b6e041aea1d14313c
RonGokhle <cips173@gmail.com>
2012-08-19 00:08:00 +0530

    CM: Add P350 (LGE Optimus Me) device
    device source : https://github.com/RonGokhle/android_device_lge_p350
    device vendor : https://github.com/RonGokhle/lge_vendor_p350
    
    Change-Id: Ie8f7430283dacadc09b10c163c51d379d8617cc6

---
+++ products/cyanogen_p350.mk
@@ -0,0 +1,29 @@
+# Include GSM stuff
+$(call inherit-product, vendor/cyanogen/products/gsm.mk)
+
+# Inherit device configuration for p350.
+$(call inherit-product, device/lge/p350/p350.mk)
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/cyanogen/products/common_full_no_themes.mk)
+
+# Include FM-Radio stuff
+$(call inherit-product, vendor/cyanogen/products/bcm_fm_radio.mk)
+
+PRODUCT_NAME := cyanogen_p350
+PRODUCT_BRAND := lge
+PRODUCT_DEVICE := p350
+PRODUCT_MODEL := LG-P350
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lge_pecan BUILD_ID=GRJ90 BUILD_FINGERPRINT=LGE/pecan/pecan:2.3.7/GRJ90/20111018:user/release-keys PRIVATE_BUILD_DESC="pecan-user 2.3.7 GRJ90 20111018 release-keys"
+
+# Extra overlay for LDPI
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/ldpi
+
+# Release name and versioning
+PRODUCT_RELEASE_NAME := OptimusMe
+PRODUCT_VERSION_DEVICE_SPECIFIC :=
+-include vendor/cyanogen/products/common_versions.mk
+
+PRODUCT_COPY_FILES +=  \
+    vendor/cyanogen/prebuilt/ldpi/media/bootanimation.zip:system/media/bootanimation.zip
